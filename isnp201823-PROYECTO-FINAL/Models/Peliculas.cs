677bf22736599eda789658f1e5c663b208ff3d10
blancoHrz <blancohrz22@gmail.com>
2024-11-06 23:45:20 -0600

    Agregar archivos de proyecto.

---
+++ isnp201823-PROYECTO-FINAL/Models/Peliculas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace isnp201823_PROYECTO_FINAL.Models
+{
+    public class Peliculas
+    {[Key]
+        public int IdPeliculas { get; set; }
+
+        public string titulo { get; set; }
+
+        public string director { get; set; }
+
+        public string sinopsis { get; set; }
+
+        public string duracion { get; set; }
+
+        public string clasificasion { get; set; }
+
+        public byte foto { get; set; }
+    }
+}
